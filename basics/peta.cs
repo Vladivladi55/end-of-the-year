67f940ef89573240be0e8eff2edc65a81092f385
Vladivladi55 <86762559+Vladivladi55@users.noreply.github.com>
2023-06-26 13:13:41 +0300

    Create peta.cs

---
+++ basics/peta.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace MyApp
+{
+    internal class Program
+    {   
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine((n-32)*5/9);
+    }
+}
